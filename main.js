7dee6efceeb87281ecbd6865a423b37c792414d3
siva <sivashakthiarun.c@gmail.com>
2022-09-13 10:03:08 +0530

    repush

---
+++ main.js
@@ -0,0 +1,7 @@
+var fabevy=["Don","Shakthi","Ajay","Wikki"];
+console.log("Original array:",fabevy);
+console.log("Length of original array:",fabevy.length);
+var a=fabevy.reverse();
+console.log("Method:fabevy.reverse();");
+console.log("Change in array:",fabevy);
+console.log("Length of array after reverse(); is ",fabevy.length);
